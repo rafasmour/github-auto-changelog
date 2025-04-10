bde33abc52cfd5b55ee86961819ed3c308884d71
rafasmour <rafasmour@gmail.com>
2025-04-10 08:37:11 +0300

    feature: added front-end folder as app

---
+++ app/src/Components/GitForm.tsx
@@ -0,0 +1,7 @@
+
+function GitForm() {
+  return (
+    <form className="flex flex-col items-center justify-center">
+        <
+    </form>
+  );}
---
+++ app/src/Layout.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+const Layout: FC = () => {
+    return (
+        <div className="min-h-screen min-w-screen bg-background">
+
+
+        </div>
+    );
+};
+
+export default Layout;
