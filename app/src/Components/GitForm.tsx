14f459754a9d1bfc4a985a0c96ce2a38ab37ff97
rafasmour <rafasmour@gmail.com>
2025-04-10 08:58:05 +0300

    feature: added text form for front-end

--- app/src/Components/GitForm.tsx
+++ app/src/Components/GitForm.tsx
@@ -1,7 +1,16 @@
+import * as React from "react";
 
-function GitForm() {
-  return (
-    <form className="flex flex-col items-center justify-center">
-        <
-    </form>
-  );}
+const GitForm: React.FC = () => {
+    return (
+        <form className="flex flex-col items-center justify-center h-screen hover:shadow-accent hover:shadow-2xl">
+            <legend className="text-3xl text-primary mb-10 text-shadow-lg text-shadow-">Git Auto Changelog</legend>
+            <div className="lex flex-col items-center justify-center">
+                <input type="text" className=" w-100 px-2 py-2 bg-secondary  radius focus:ring-accent focus:ring-2 focus:outline-none focus:ring-l-0"/>
+                <input type="submit"  />
+            </div>
+
+        </form>
+    );
+}
+
+export default GitForm;
--- app/src/Layout.tsx
+++ app/src/Layout.tsx
@@ -1,10 +1,10 @@
 import { FC } from 'react';
+import GitForm from "./Components/GitForm.tsx";
 
 const Layout: FC = () => {
     return (
         <div className="min-h-screen min-w-screen bg-background">
-
-
+            <GitForm />
         </div>
     );
 };
